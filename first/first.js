1d2534e83093d5505e3cbd1cca88fb8cae525117
Devansh Pandey <pandeydevanshhhh@gmail.com>
2026-04-18 14:16:06 +0530

    19/04/2026 COMMIT

---
+++ first/first.js
@@ -0,0 +1,56 @@
+/*const product={
+    title:"Ball Pen",
+    rating:4,
+    offer:5,
+    price:270,
+}
+console.log(product);*/
+
+// const profile={
+//     username:"devanshpandey0",
+//     isFollow:false,
+//     followers:1000,
+//     following:500,
+
+// }
+// console.log(profile);
+
+// unary operator
+/*let a=5;
+let b=2;
+console.log ("a=",a,"b=",b);
+
+console.log("a--=",a--);
+console.log("a=",a)*/
+
+//assignment operator
+// let a=5;
+// let b=2;
+
+// a/=4; 
+// console.log("a=",a);
+
+// comparison operator
+// let a=5;
+// let b=2; 
+// console.log("a==b",a==b);
+
+//logical operator
+// let a=5;
+// let b=2;     
+// let cond1=a>b;  
+// let cond2=a==5;
+// console.log("cond1 && cond2",cond1 && cond2);
+// console.log("cond1 || cond2",cond1 || cond2);
+
+// for(let i=1;i<=5;i++){
+//     console.log("hello world",i);
+// }
+
+// calculate the sum of first n natural numbers
+let n=10;
+let sum=0;
+for(let i=1;i<=n;i++){
+    sum+=i;
+}
+console.log("Sum of first",n,"natural numbers is:",sum);
